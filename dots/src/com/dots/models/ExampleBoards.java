00f55444fca890b9f6e074cd635962b420588da6
eswedick <swedicem@clarkson.edu>
2013-12-03 20:42:13 -0500

    changed example board 2 to a look ahead case

--- dots/src/com/dots/models/ExampleBoards.java
+++ dots/src/com/dots/models/ExampleBoards.java
@@ -71,17 +71,17 @@ public class ExampleBoards {
 
         // Column 0
         boardModel.setDot(new DotModel(0, 0, Color.BLUE));
-        boardModel.setDot(new DotModel(0, 1, Color.RED));
-        boardModel.setDot(new DotModel(0, 2, Color.MAGENTA));
+        boardModel.setDot(new DotModel(0, 1, Color.GREEN));
+        boardModel.setDot(new DotModel(0, 2, Color.RED));
         boardModel.setDot(new DotModel(0, 3, Color.RED));
         boardModel.setDot(new DotModel(0, 4, Color.GREEN));
         boardModel.setDot(new DotModel(0, 5, Color.BLUE));
 
         // Column 1
         boardModel.setDot(new DotModel(1, 0, Color.YELLOW));
-        boardModel.setDot(new DotModel(1, 1, Color.MAGENTA));
-        boardModel.setDot(new DotModel(1, 2, Color.GREEN));
-        boardModel.setDot(new DotModel(1, 3, Color.RED));
+        boardModel.setDot(new DotModel(1, 1, Color.RED));
+        boardModel.setDot(new DotModel(1, 2, Color.RED));
+        boardModel.setDot(new DotModel(1, 3, Color.YELLOW));
         boardModel.setDot(new DotModel(1, 4, Color.BLUE));
         boardModel.setDot(new DotModel(1, 5, Color.GREEN));
 
@@ -99,29 +99,29 @@ public class ExampleBoards {
         boardModel.setDot(new DotModel(3, 2, Color.YELLOW));
         boardModel.setDot(new DotModel(3, 3, Color.YELLOW));
         boardModel.setDot(new DotModel(3, 4, Color.GREEN));
-        boardModel.setDot(new DotModel(3, 5, Color.BLUE));
+        boardModel.setDot(new DotModel(3, 5, Color.RED));
 
         // Column 4
         boardModel.setDot(new DotModel(4, 0, Color.BLUE));
         boardModel.setDot(new DotModel(4, 1, Color.GREEN));
-        boardModel.setDot(new DotModel(4, 2, Color.MAGENTA));
-        boardModel.setDot(new DotModel(4, 3, Color.MAGENTA));
+        boardModel.setDot(new DotModel(4, 2, Color.RED));
+        boardModel.setDot(new DotModel(4, 3, Color.RED));
         boardModel.setDot(new DotModel(4, 4, Color.YELLOW));
         boardModel.setDot(new DotModel(4, 5, Color.MAGENTA));
 
         // Column 5
         boardModel.setDot(new DotModel(5, 0, Color.BLUE));
         boardModel.setDot(new DotModel(5, 1, Color.BLUE));
-        boardModel.setDot(new DotModel(5, 2, Color.YELLOW));
-        boardModel.setDot(new DotModel(5, 3, Color.YELLOW));
+        boardModel.setDot(new DotModel(5, 2, Color.BLUE));
+        boardModel.setDot(new DotModel(5, 3, Color.BLUE));
         boardModel.setDot(new DotModel(5, 4, Color.GREEN));
         boardModel.setDot(new DotModel(5, 5, Color.MAGENTA));
 
-        ArrayList<DotModel> selectedDots = new ArrayList<DotModel>();
-        selectedDots.add(boardModel.getDot(4, 5));
-        selectedDots.add(boardModel.getDot(5, 5));
-        SelectionModel selectionModel = new SelectionModel(boardModel, selectedDots);
-        boardModel.setSelectionModel(selectionModel);
+        //ArrayList<DotModel> selectedDots = new ArrayList<DotModel>();
+        //selectedDots.add(boardModel.getDot(4, 5));
+        //selectedDots.add(boardModel.getDot(5, 5));
+        //SelectionModel selectionModel = new SelectionModel(boardModel, selectedDots);
+        boardModel.setSelectionModel(null);
 
         return boardModel;
     }
